17df1fdefcddce8ca9152e76616d816b902f844c
thomas <nealthom@gmail.com>
2018-04-20 14:04:36 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+
+const app = express();
+app.use(express.static(publicPath));
+
+
+app.get('/',(req,res)=>{
+  res.render('index.html');
+});
+
+app.listen(port,()=>{
+  console.log(`Server is up on ${port}`);
+})
